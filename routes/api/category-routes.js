b8ae96cea19cc86b854d5d814985800c2fdbc6e1
AlexMastroianni <alex.m.mastroianni@gmail.com>
2022-10-31 16:22:26 +1100

    refactor product/category routers

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,7 +14,7 @@ router.get("/", (req, res) => {
   })
     .then((categoryDB) => {
       if (!categoryDB) {
-        res.status(404).json({ message: "No categories found" });
+        res.status(404).json({ message: "Categories not found" });
         return;
       }
       res.json(categoryDB);
@@ -39,7 +39,7 @@ router.get("/:id", (req, res) => {
   })
     .then((categoryDB) => {
       if (!categoryDB) {
-        res.status(404).json({ message: "No categories found" });
+        res.status(404).json({ message: "Categories not found" });
         return;
       }
       res.json(categoryDB);
@@ -71,7 +71,7 @@ router.put("/:id", (req, res) => {
   })
     .then((categoryDB) => {
       if (!categoryDB) {
-        res.status(404).json({ message: "No category found with this id" });
+        res.status(404).json({ message: "Categories not found" });
         return;
       }
       res.json(categoryDB);
@@ -91,7 +91,7 @@ router.delete("/:id", (req, res) => {
   })
     .then((categoryDB) => {
       if (!categoryDB) {
-        res.status(404).json({ message: "No category found with that id." });
+        res.status(404).json({ message: "Categories not found" });
         return;
       }
       res.json(categoryDB);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,18 +7,28 @@ const { Product, Category, Tag, ProductTag } = require("../../models");
 router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  Category.findAll({
+  Product.findAll({
     include: {
       model: Product,
       attributes: ["id", "product_name", "price", "stock", "category_id"],
     },
+    include: [
+      {
+        model: Category,
+        attributes: ["category_name"],
+      },
+      {
+        model: Tag,
+        attributes: ["tag_name"],
+      },
+    ],
   })
-    .then((categoryDB) => {
-      if (!categoryDB) {
-        res.status(404).json({ message: "No categories found" });
+    .then((productDB) => {
+      if (!productDB) {
+        res.status(404).json({ message: "Products not found" });
         return;
       }
-      res.json(categoryDB);
+      res.json(productDB);
     })
     .catch((err) => {
       console.log(err);
@@ -39,12 +49,12 @@ router.get("/:id", (req, res) => {
       attributes: ["id", "product_name", "price", "stock", "category_id"],
     },
   })
-    .then((categoryDB) => {
-      if (!categoryDB) {
-        res.status(404).json({ message: "No categories found" });
+    .then((productDB) => {
+      if (!productDB) {
+        res.status(404).json({ message: "Products not found" });
         return;
       }
-      res.json(categoryDB);
+      res.json(productDB);
     })
     .catch((err) => {
       console.log(err);
@@ -54,15 +64,13 @@ router.get("/:id", (req, res) => {
 
 // create new product
 router.post("/", (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+  Product.create(req.body)({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tagIds,
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -133,12 +141,12 @@ router.delete("/:id", (req, res) => {
       id: req.params.id,
     },
   })
-    .then((categoryDB) => {
-      if (!categoryDB) {
-        res.status(404).json({ message: "No category found with that id." });
+    .then((productDB) => {
+      if (!productDB) {
+        res.status(404).json({ message: "Products not found" });
         return;
       }
-      res.json(categoryDB);
+      res.json(productDB);
     })
     .catch((err) => {
       console.log(err);
